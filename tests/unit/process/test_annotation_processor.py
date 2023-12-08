8b486a753e6c35633e32c92a5e1fb40452ca39c7
Vincent Menger <vmenger@protonmail.com>
2023-12-08 14:36:27 +0100

    Formatting

--- tests/unit/process/test_annotation_processor.py
+++ tests/unit/process/test_annotation_processor.py
@@ -3,7 +3,10 @@ import pytest
 from frozendict import frozendict
 
 from docdeid.annotation import Annotation, AnnotationSet
-from docdeid.process.annotation_processor import MergeAdjacentAnnotations, OverlapResolver
+from docdeid.process.annotation_processor import (
+    MergeAdjacentAnnotations,
+    OverlapResolver,
+)
 
 
 class TestOverlapResolver:
